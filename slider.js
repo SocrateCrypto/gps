95df83d581fea7973cbbc79bc5f7d1cec0f143ce
Anatoly <80717919+SocrateCrypto@users.noreply.github.com>
2025-02-11 20:14:00 +0100

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,39 @@
+var clickPipsSlider = document.getElementById('range');
+
+noUiSlider.create(clickPipsSlider, {
+    range: {
+        min: 100,
+        max: 2000
+    },
+    connect: 'lower',
+    start: [1000],
+    pips: {mode: 'count', values: 5},
+    tooltips: [
+        // no tooltip
+        true,
+        // tooltip with custom formatting
+       
+    ],
+   
+       
+       
+    
+});
+
+var pips = clickPipsSlider.querySelectorAll('.noUi-value');
+
+function clickOnPip() {
+    var value = Number(this.getAttribute('data-value'));
+    clickPipsSlider.noUiSlider.set(value);
+}
+
+for (var i = 0; i < pips.length; i++) {
+
+    // For this example. Do this in CSS!
+    pips[i].style.cursor = 'pointer';
+    pips[i].addEventListener('click', clickOnPip);
+}
+
+
+
+
